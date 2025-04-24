afce5c08306b22e48b218076bba69d6fbc444456
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2025-04-25 01:11:19 +0300

    Fix linter config and errors

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,17 +1,12 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from 'express';
 
 export interface AppError extends Error {
-    status?: number;
-  }
-  
-  export const errorHandler = (
-    err: AppError,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    console.error(err);
-    res.status(err.status || 500).json({
-      message: err.message || 'Internal Server Error',
-    });
-  };
+  status?: number;
+}
+
+export const errorHandler = (err: AppError, req: Request, res: Response) => {
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+};
