7eb37a769ea4856bccf6d8930fc99f66e5c2238a
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2025-04-25 01:22:41 +0300

    Replace ts-node with tsx and fix error handler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,10 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export interface AppError extends Error {
   status?: number;
 }
 
-export const errorHandler = (err: AppError, req: Request, res: Response) => {
+export const errorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction, // eslint-disable-line @typescript-eslint/no-unused-vars
+) => {
   console.error(err);
   res.status(err.status || 500).json({
     message: err.message || 'Internal Server Error',
